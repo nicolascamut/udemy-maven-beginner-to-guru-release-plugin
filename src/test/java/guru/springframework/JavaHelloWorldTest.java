7d86562cbef17a7408939c0a9b6f01d1479e3e55
Nicolas Camut <nicolas.camut@paf.com>
2024-09-30 18:53:53 +0300

    Break a test to make the build fail

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -2,11 +2,13 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 class JavaHelloWorldTest {
 
     @Test
     void getHello() {
         System.out.println(new JavaHelloWorld().getHello());
-
+        fail("Made to fail!");
     }
 }
